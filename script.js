222dabecd8b2e35861fbb5f7b5e124d2412ba7d5
Smael Nicolas <nico.smael@gmail.com>
2023-08-02 18:08:32 -0300

    intersection observer

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("HOLA");
+const title = document.querySelector("#titulo");
+
+const callMe = (entries) => {
+	let entry = entries[0];
+	if (entry.isIntersecting) {
+		title.classList.add("active");
+	} else {
+		title.classList.remove("active");
+	}
+};
+
+let options = {};
+
+let observer = new IntersectionObserver(callMe, options);
+
+observer.observe(title);

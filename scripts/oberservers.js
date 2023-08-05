94d5d9c45fa8f8dbeaa1b787d9c424690c475793
Smael Nicolas <nico.smael@gmail.com>
2023-08-05 19:19:52 -0300

    services section

--- scripts/oberservers.js
+++ scripts/oberservers.js
@@ -1,10 +1,21 @@
 const options = {
 	root: null,
 	threshold: 1,
-	rootMargin: "0px 0px -80px 0px",
+	rootMargin: "0px 0px -60px 0px",
 };
 export const observersElements = () => {
 	const title = document.querySelector("#titulo");
+	const imagesLazy = document.querySelectorAll(".lazy");
+
+	const addUrl = (entries, obs) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.src = entry.target.dataset.src;
+				entry.target.classList.remove("lazy");
+				obs.unobserve(entry.target);
+			}
+		});
+	};
 
 	const callMe = (entries) => {
 		let entry = entries[0];
@@ -16,6 +27,11 @@ export const observersElements = () => {
 	};
 
 	let observer = new IntersectionObserver(callMe, options);
+	let observerImages = new IntersectionObserver(addUrl, options);
 
 	observer.observe(title);
+
+	imagesLazy.forEach((image) => {
+		observerImages.observe(image);
+	});
 };
